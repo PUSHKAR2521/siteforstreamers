074c8ee2a16f7f03ceae0214a376a50e2e0e9d19
B A C <117440896+PUSHKAR2521@users.noreply.github.com>
2023-08-26 18:27:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Streamers Place | Pushkar</title>
+    <link href="" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Tipping Page</a>
+          </li>
+          <li>
+            <a href="#">Streamer Support</a>
+          </li>
+      </nav>
+    </header>
+  </body>
+</html>
